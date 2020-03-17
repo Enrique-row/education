9a3ec200b9b4277dfb8a0d9e40ca8330d36b3603
fimorr <fimorrr@yandex.ru>
2020-03-17 17:00:41 +0300

    Updates

---
+++ precache-manifest.8c56a8a616a3c3703afe6e1e180f8761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd9ed6bccc2a0d869fd6f960ee248dd",
+    "url": "/education/index.html"
+  },
+  {
+    "revision": "0a96b0f17a9bdb4df93d",
+    "url": "/education/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "f6d2e72750e9a168086f",
+    "url": "/education/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "0a96b0f17a9bdb4df93d",
+    "url": "/education/static/js/2.54a61c23.chunk.js"
+  },
+  {
+    "revision": "f6d2e72750e9a168086f",
+    "url": "/education/static/js/main.3481c01a.chunk.js"
+  },
+  {
+    "revision": "b39f34f690a6a9a6fa6a",
+    "url": "/education/static/js/runtime-main.2f498681.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/education/static/media/persik.4e1ec840.png"
+  }
+]);
